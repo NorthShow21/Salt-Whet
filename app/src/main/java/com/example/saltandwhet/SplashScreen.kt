4b9e430330e946197935d61b11176054074a7ae7
Joel <vynandrog@gmail.com>
2025-06-16 15:49:27 +0700

    commit 1

---
+++ app/src/main/java/com/example/saltandwhet/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.example.saltandwhet
+
+import android.os.Bundle
+import android.text.Html
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.splash_screen)
+        val textView = findViewById<TextView>(R.id.textViewTitle)
+        val styledText = "Find the perfect recipe with <font color='#FFEB3B'><b>Salt and Whet</b></font>"
+        textView.text = Html.fromHtml("<b>$styledText</b>", Html.FROM_HTML_MODE_LEGACY)
+    }
+}
